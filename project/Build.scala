f8849e82f3adfa9f80d57796e52737092f34073e
Sungho.Ryu <shryu@NAVER.COM>
2012-11-13 11:10:13 +0900

    Changed dependencies for algebird & chill to use HTTP uri scheme.

--- project/Build.scala
+++ project/Build.scala
@@ -2,6 +2,6 @@ import sbt._
 
 object ScaldingBuild extends Build {
   lazy val root = Project("root", file("."))
-                    .dependsOn(RootProject(uri("git://github.com/twitter/algebird.git#master")))
-                    .dependsOn(RootProject(uri("git://github.com/twitter/chill.git#master")))
+                    .dependsOn(RootProject(uri("http://github.com/twitter/algebird.git#master")))
+                    .dependsOn(RootProject(uri("http://github.com/twitter/chill.git#master")))
 }
